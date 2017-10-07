506401d404a4582e97981be81fb123786d4cfa68
Sebastian Fischer <typo3@evoweb.de>
2017-10-07 08:34:37 +0200

    Add twitter connection and query return

--- Classes/Controller/DisplayController.php
+++ Classes/Controller/DisplayController.php
@@ -1,9 +1,9 @@
 <?php
 namespace Evoweb\EwSocialfeedwall\Controller;
 
-use TYPO3\CMS\Core\Page\PageRenderer;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
-
+/**
+ * Class DisplayController
+ */
 class DisplayController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
     protected function initializeAction()
@@ -16,14 +16,16 @@ class DisplayController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
     protected function addResources()
     {
         $configuration = $this->configurationManager
-            ->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK)['view'];
+            ->getConfiguration(
+                \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK
+            )['view'];
 
         if (empty($configuration)) {
             return;
         }
 
-        /** @var PageRenderer $pageRenderer */
-        $pageRenderer = $this->objectManager->get(PageRenderer::class);
+        /** @var \TYPO3\CMS\Core\Page\PageRenderer $pageRenderer */
+        $pageRenderer = $this->objectManager->get(\TYPO3\CMS\Core\Page\PageRenderer::class);
 
         if (isset($configuration['cssFiles']) && is_array($configuration['cssFiles'])) {
             array_walk($configuration['cssFiles'], function ($value) use ($pageRenderer) {
@@ -48,6 +50,32 @@ class DisplayController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
 
     public function getTweetsAction()
     {
-        #use \Abraham\TwitterOAuth\TwitterOAuth;
+        $twitterConfiguration = $this->settings['twitter'];
+
+        $parameter = [
+            'q' => $this->request->getArgument('search'),
+            'count' => $this->settings['limit'],
+            'exclude_replies' => true,
+        ];
+
+        if ($this->request->hasArgument('since_id')) {
+            $parameter['since_id'] = $this->request->getArgument('since_id');
+        }
+
+        /** @var \Abraham\TwitterOAuth\TwitterOAuth $connection */
+        /** @noinspection PhpMethodParametersCountMismatchInspection */
+        $connection = $this->objectManager->get(
+            \Abraham\TwitterOAuth\TwitterOAuth::class,
+            $twitterConfiguration['consumer_key'],
+            $twitterConfiguration['consumer_secret'],
+            $twitterConfiguration['access_token'],
+            $twitterConfiguration['access_token_secret']
+        );
+
+        $statuses = $connection->get('search/tweets', $parameter);
+
+        $this->request->setFormat('json');
+        $jsonResult = str_replace('\/', '/', \GuzzleHttp\json_encode($statuses->statuses));
+        return $jsonResult;
     }
 }
