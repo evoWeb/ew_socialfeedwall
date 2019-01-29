3cb68728c1251ba265d5a66d4c23c8002598d0be
Sebastian Fischer <typo3@evoweb.de>
2019-01-29 20:05:54 +0100

    Cleanup build files

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,16 +4,15 @@ $EM_CONF['ew_socialfeedwall'] = [
     'title' => 'Social feed wall',
     'description' => 'Displays social feeds like twitter wall',
     'category' => 'misc',
-    'version' => '1.0.1',
-    'state' => 'stable',
-    'clearcacheonload' => 1,
     'author' => 'Sebastian Fischer',
     'author_email' => 'typo3@evoweb.de',
     'author_company' => 'evoweb',
+    'state' => 'stable',
+    'clearcacheonload' => 1,
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.2.99',
+            'typo3' => '8.7.0-9.5.99',
         ],
-        'conflicts' => [],
     ],
 ];
